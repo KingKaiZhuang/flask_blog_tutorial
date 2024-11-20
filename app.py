22fc0534805a7e6886133f452bc1ccb3f6cfe70b
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-11-20 14:36:03 +0800

    add forms.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,flash
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b0c5c7b63636ba8322a3c10af3b43db8'
 
 posts = [
     {
@@ -26,6 +29,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register",methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!' , 'success')
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
